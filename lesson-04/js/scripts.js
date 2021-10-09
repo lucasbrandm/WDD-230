6c8c404e1c1bebfb37ee3f599534b699432cafe2
lucas <mor18008@byui.edu>
2021-10-09 14:28:24 -0600

    Finished the large viewport

---
+++ lesson-04/js/scripts.js
@@ -0,0 +1,32 @@
+d = new Date()
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+const monthName = months[d.getMonth()]
+const dayName = days[d.getDay()]
+
+formatted_date = dayName + ', ' + d.getDay() + " " + monthName + " " + d.getFullYear();
+document.getElementById("date").innerHTML = formatted_date;
