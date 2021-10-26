94c1a7b6ee0be855551529de207ebff769d04c25
lucas <mor18008@byui.edu>
2021-10-26 09:17:44 -0600

    Doing some LazLoading stuff

--- lesson-06/js/windchill.js
+++ lesson-06/js/windchill.js
@@ -11,6 +11,9 @@ if ((high.textContent >= 50) && (wind.textContent >= 3)) {
     var s = wind.textContent;
     s = Math.pow(s, 0.16);
     var f = 35.74 + (0.6215 * t) - (35.75 * s) + (0.4275 * t * s);
-    chill.textContent = f.toFixed()
-    console.log(f)
+    chill.textContent = f.toFixed() + " °F";
+    console.log(f);
+}
+else {
+    chill.textContent = "N/A";
 }
